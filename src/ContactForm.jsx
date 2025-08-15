720434c7c061316f897c975d3ab03c6433630925
sulimanshams <sulimanshamsn191@gmail.com>
2025-08-15 13:35:20 +0430

    Contact Form some changes but not completly

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -7,16 +7,19 @@ export default function ContactForm(){
      return (
         <Box>
             <Typography variant="h6" sx={{fontWeight:880, mb:2}}>Contact Information</Typography>
-            <Stack spacing={2}>
-                <TextField fullWidth label="First & last name " placeholder="Enter first & last name" InputProps={{sx:{borderRadius:2}}} />
+            <Stack spacing={2} sx={{width:"100%"}}>
+                <Typography variant="body2" sx={{mb:"0.5" , fontWeight:600}}>First & Last name</Typography>
+                <TextField fullWidth  placeholder="Enter first & last name" sx={{minWidth:"500px"}} InputProps={{sx:{borderRadius:2 }}} />
+                <Typography variant="body2" sx={{mb:"0.5" , fontWeight:600}}>Email Address</Typography>
                 <TextField fullWidth type="email" label="Email Address " placeholder="Enter Email Address" InputProps={{sx:{borderRadius:2}}} /> 
                 
 
 
                 <Box sx={{display:"grid" , gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr" } , gap:2}}>
                     <FormControl fullWidth >
-                        <InputLabel>Country</InputLabel>
-                        <Select label="Country" defaultValue="Fenland" sx={{borderRadius:2}}>
+                        {/* <InputLabel>Country</InputLabel> */}
+                        <Typography variant="body2" sx={{mb:"0.5" , fontWeight:600}}>Country</Typography>
+                        <Select  defaultValue="Fenland" sx={{borderRadius:2}}>
                                 <MenuItem value="Finland">Finland</MenuItem>
                                 <MenuItem value="Afghanistan">Afghanistan</MenuItem>
                                 <MenuItem value="United Kingdom">United Kingdom</MenuItem>
@@ -24,10 +27,17 @@ export default function ContactForm(){
                                 <MenuItem value="Russia">Russia</MenuItem>
                         </Select>
                     </FormControl>
-                           <TextField label="Postal Card" placeholder="00270" InputProps={{sx:{borderRadius:2}}} />
+                    <Box>
 
-                           
+                                <Typography variant="body2" sx={{mb:"0.5" , fontWeight:600}}>Postal Card</Typography>
+                                <TextField  placeholder="00270" InputProps={{sx:{borderRadius:2}}} />
+                    </Box>
+
+                          
                 </Box>
+                 <Button variant="contained" size="large" sx={{mt:0.5 , width:"500px" , bgcolor:"#111" ,"&:hover": { bgcolor: "#000" } , borderRadius:2 , py:1.25 , fontWeight:700}}>
+                               Continue
+                           </Button>
             </Stack>
         </Box>
      )
